969cf3dad4721261d7fff76347df2e8adc26922d
SirraRakesh <sirrarakesh2000@gmail.com>
2023-10-30 10:22:13 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,45 +1,63 @@
-
-import './App.css';
-import { useState } from 'react'
-import Taskitems from './tasks/Taskitems';
+import "./App.css";
+import { useState } from "react";
+import Taskitems from "./tasks/Taskitems";
 function App() {
-  const [task, setTask] = useState("")
-  const [todo, setTodo] = useState([])
-  const changeHandler = (e) => { setTask(e.target.value) }
-  const submithandler = e => {
+  const [task, setTask] = useState("");
+  const [todo, setTodo] = useState([]);
+  const [items, setItems] = useState([]);
+  const changeHandler = (e) => {
+    setTask(e.target.value);
+  };
+  const submithandler = (e) => {
     e.preventDefault();
-    const len=e.target.firstChild.value.trim().length 
-   if(len>0)
-   {todo.push([task, Date.now()])}
+    const len = e.target.firstChild.value.trim().length;
+    if (len > 0) {
+      todo.push([task, Date.now()]);
+      // setItem({...item,task,Date.now()})
+      setItems([...todo]);
+      // console.log(JSON.stringify(items));
 
-   else
-   {todo=todo}
-    
-    setTask('')
-  }
-  const delhandler = (indexValue) => {
-    const newTodo = todo.filter((ele, index) => index !== indexValue);
-    setTodo(newTodo)
-  }
-  
+      localStorage.setItem("items", JSON.stringify([...todo]));
+    } else {
+      todo = todo;
+    }
+
+    setTask("");
+  };
+  const delhandler = (indexValue, parsedItem) => {
+    // console.log(parsedItem);
+    const newTodo = parsedItem.filter((ele, index) => index !== indexValue);
+
+    setTodo(newTodo);
+    // console.log(parsedItem[indexValue]);
+    // localStorage.removeItem(parsedItem);
+    // parsedItem = [...newTodo];
+    // console.log(parsedItem);
+  };
+
   return (
-    <div className='container'>
-      <div className='title'>
+    <div className="container">
+      <div className="title">
         <h1>To-Do-List</h1>
       </div>
-      <div className='input-text'>
+      <div className="input-text">
         <form onSubmit={submithandler}>
-          <input className='typing'
-            type='text'
+          <input
+            className="typing"
+            type="text"
             placeholder="enter items in to  todo list"
             value={task}
-            onChange={changeHandler}>
-          </input>
-          <input type='submit' className='add-btn'
-            value='add'
-            name='add'></input></form>
+            onChange={changeHandler}
+          ></input>
+          <input
+            type="submit"
+            className="add-btn"
+            value="add"
+            name="add"
+          ></input>
+        </form>
       </div>
-      <Taskitems todolist={todo} delhandlerfun={delhandler} />
+      <Taskitems todolist={todo} items={items} delhandlerfun={delhandler} />
     </div>
   );
 }
--- src/tasks/Taskitems.jsx
+++ src/tasks/Taskitems.jsx
@@ -1,26 +1,46 @@
-import './taskitems.css'
-function Taskitems({ todolist, delhandlerfun, doneHandler }) {
+import "./taskitems.css";
+function Taskitems({ todolist, items, delhandlerfun, doneHandler }) {
+  // const savedItem = localStorage.getItem("items");
+  // const parsedItem = JSON.parse(savedItem);
+  // console.log(parsedItem);
   const styles = (e) => {
     e.target.innerText = "Ongoing";
-    e.target.id = 'notdone';
-  }
-  const styles1 = (e) => { e.target.innerText = "Done"; e.target.id = 'done' }
+    e.target.id = "notdone";
+  };
+  const styles1 = (e) => {
+    e.target.innerText = "Done";
+    e.target.id = "done";
+  };
   return (
-    <div id='items-container'>
-      {todolist.map((item, index) =>
-        <div key={index} id='each-item'>
+    <div id="items-container">
+      {todolist.map((item, index, parsedItem) => (
+        <div key={index} id="each-item">
           <span className="taskitem">{item[0]}</span>
-          <div id='itembuttons'>
-            <button className='del' onClick={() => delhandlerfun(index)}>Del</button>
-            <button onDoubleClick={styles} onClick={styles1}>Done</button></div>
-          <span id='timestamp'>{(new Intl.DateTimeFormat('en-US', { day: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit', month: '2-digit', }).format(item[1]))}</span>
+          <div id="itembuttons">
+            <button
+              className="del"
+              onClick={() => delhandlerfun(index, parsedItem)}
+            >
+              Del
+            </button>
+            <button onDoubleClick={styles} onClick={styles1}>
+              Done
+            </button>
+          </div>
+          <span id="timestamp">
+            {new Intl.DateTimeFormat("en-US", {
+              day: "2-digit",
+              year: "numeric",
+              hour: "2-digit",
+              minute: "2-digit",
+              second: "2-digit",
+              month: "2-digit",
+            }).format(item[1])}
+          </span>
         </div>
-      )}
-
+      ))}
     </div>
   );
 }
 
 export default Taskitems;
-
-
